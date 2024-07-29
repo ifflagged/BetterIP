da1769c499025f9c4d281cf8cf0a6dc862cb1640
ifflagged <91680218+ifflagged@users.noreply.github.com>
2024-07-29 15:50:03 +0800

    Update Signin-NS.py

--- Signin-NS.py
+++ Signin-NS.py
@@ -3,12 +3,16 @@ import os
 import sys
 from curl_cffi import requests
 
-NS_RANDOM = os.environ.get("NS_RANDOM", "false")
-NS_COOKIES = os.environ.get("NS_COOKIES", "")
-telegram_bot_token = os.environ.get("TELEGRAM_BOT_TOKEN", "")
-chat_id = os.environ.get("CHAT_ID", "")
+NS_RANDOM = os.environ.get("NS_RANDOM","false")
+NS_COOKIE = os.environ.get("NS_COOKIE","")
+COOKIE = os.environ.get("COOKIE", "")
+COOKIE_ENV = NS_COOKIE or COOKIE
 
-def telegram_Bot(token, chat_id, message):
+pushplus_token = os.environ.get("PUSHPLUS_TOKEN")
+telegram_bot_token = os.environ.get("TELEGRAM_BOT_TOKEN","")
+chat_id = os.environ.get("CHAT_ID","")
+telegram_api_url = os.environ.get("TELEGRAM_API_URL","https://api.telegram.org") # 代理api,可以使用自己的反代
+def telegram_Bot(token,chat_id,message):
     url = f'{telegram_api_url}/bot{token}/sendMessage'
     data = {
         'chat_id': chat_id,
@@ -18,9 +22,8 @@ def telegram_Bot(token, chat_id, message):
     response_data = r.json()
     msg = response_data['ok']
     print(f"telegram推送结果：{msg}\n")
-
 def pushplus_ts(token, rw, msg):
-    url = 'https://www.pushplus.plus/send/';
+    url = 'https://www.pushplus.plus/send/'
     data = {
         "token": token,
         "title": rw,
@@ -38,36 +41,36 @@ def load_send():
     if os.path.exists(cur_path + "/notify.py"):
         try:
             from notify import send
-            hadsend = True
+            hadsend=True
         except:
             print("加载notify.py的通知服务失败，请检查~")
-            hadsend = False
+            hadsend=False
     else:
         print("加载通知服务失败,缺少notify.py文件")
-        hadsend = False
-
+        hadsend=False
 load_send()
 
-def sign_in(cookie):
-    url = f"https://www.nodeseek.com/api/attendance?random={NS_RANDOM}";
+if COOKIE_ENV:
+    url = f"https://www.nodeseek.com/api/attendance?random={NS_RANDOM}"
     headers = {
         'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0",
         'sec-ch-ua': "\"Not A(Brand\";v=\"99\", \"Microsoft Edge\";v=\"121\", \"Chromium\";v=\"121\"",
         'sec-ch-ua-mobile': "?0",
         'sec-ch-ua-platform': "\"Windows\"",
-        'origin': "https://www.nodeseek.com";,
+        'origin': "https://www.nodeseek.com",
         'sec-fetch-site': "same-origin",
         'sec-fetch-mode': "cors",
         'sec-fetch-dest': "empty",
-        'referer': "https://www.nodeseek.com/board";,
+        'referer': "https://www.nodeseek.com/board",
         'accept-language': "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
-        'Cookie': cookie
+        'Cookie': COOKIE_ENV
     }
 
     try:
-        response = requests.post(url, headers=headers, impersonate="chrome110")
+        response = requests.post(url, headers=headers,impersonate="chrome110")
         response_data = response.json()
         print(response_data)
+        print(COOKIE_ENV)
         message = response_data.get('message')
         success = response_data.get('success')
         send("nodeseek签到", message)
@@ -84,10 +87,5 @@ def sign_in(cookie):
     except Exception as e:
         print("发生异常:", e)
         print("实际响应内容:", response.text)
-
-if NS_COOKIES:
-    cookies = NS_COOKIES.split(',')
-    for cookie in cookies:
-        sign_in(cookie)
 else:
-    print("请先设置NS_COOKIES")
+    print("请先设置Cookie")
