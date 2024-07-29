0bc666f163a8ec432298372d55e6c04db1199541
ifflagged <91680218+ifflagged@users.noreply.github.com>
2024-07-29 15:43:53 +0800

    Update Signin-NS.py

--- Signin-NS.py
+++ Signin-NS.py
@@ -7,7 +7,6 @@ NS_RANDOM = os.environ.get("NS_RANDOM", "false")
 NS_COOKIES = os.environ.get("NS_COOKIES", "")
 telegram_bot_token = os.environ.get("TELEGRAM_BOT_TOKEN", "")
 chat_id = os.environ.get("CHAT_ID", "")
-telegram_api_url = os.environ.get("TELEGRAM_API_URL", "https://api.telegram.org";)  # 代理api,可以使用自己的反代
 
 def telegram_Bot(token, chat_id, message):
     url = f'{telegram_api_url}/bot{token}/sendMessage'
