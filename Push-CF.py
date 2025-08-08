7e80895d7cea7196ba902f73e8f6017cfabe4b4b
ifflagged <91680218+ifflagged@users.noreply.github.com>
2025-08-08 10:22:48 +0800

    Update Push-CF.py

--- Push-CF.py
+++ Push-CF.py
@@ -2,7 +2,6 @@ import requests
 import traceback
 import time
 import os
-import json
 
 # API 密钥
 CF_API_TOKEN    =   os.environ["CF_API_TOKEN"]
@@ -69,14 +68,11 @@ def main():
     ip_addresses_str = get_cf_speed_test_ip()
     ip_addresses = ip_addresses_str.split(',')
     dns_records = get_dns_records(CF_DNS_NAME)
-    push_plus_content = []
     # 遍历 IP 地址列表
     for index, ip_address in enumerate(ip_addresses):
         # 执行 DNS 变更
         dns = update_dns_record(dns_records[index], CF_DNS_NAME, ip_address)
-        push_plus_content.append(dns)
-
-    push_plus('\n'.join(push_plus_content))
+        print(dns)  # 输出到控制台，不再发送推送
 
 if __name__ == '__main__':
     main()
